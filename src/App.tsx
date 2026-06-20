564fa0ae895eb80fb321f6b1441af1e2663d2e8f
happydetourtravel <happydetourtravel@gmail.com>
2026-06-20 02:15:39 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Home from './pages/home/page'
 import Join from './pages/join/page'
 import Login from './pages/login/page'
+import Privacy from './pages/privacy/page'
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/join" element={<Join />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/privacy" element={<Privacy />} />
         </Routes>
         <Footer />
       </div>
