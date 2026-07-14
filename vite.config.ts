be59dcd5dd798c68962175702947dadaa03d1797
happydetourtravel <happydetourtravel@gmail.com>
2026-07-14 11:46:26 -0400

    Add SSG options to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.md'],
+  ssgOptions: {
+    includedRoutes(paths) {
+      const skip = ['/login', '/join', '/verify', '/members']
+      return paths.filter((path) => !skip.includes(path))
+    },
+  },
 })
